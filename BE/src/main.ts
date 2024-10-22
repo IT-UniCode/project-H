3ef7217e7f0fcdcac93c55e2b2169f64aeb56721
Oleksiy Chernenko <oleksiychernenko286@gmail.com>
2024-10-22 13:19:30 +0300

    configuration cors and port for proxy

--- BE/src/main.ts
+++ BE/src/main.ts
@@ -12,10 +12,11 @@ async function bootstrap() {
     .build();
 
   // app.setGlobalPrefix('api');
+  app.enableCors();
 
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
--- BE/src/request/request.service.ts
+++ BE/src/request/request.service.ts
@@ -27,8 +27,6 @@ export class RequestService {
 
       return res;
     } catch (error) {
-      console.log(error);
-
       throw new ForbiddenException(exception);
     }
   }
