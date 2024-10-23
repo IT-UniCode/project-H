f8db8e6cdafe1c395946b762b5ee607de7cb2b0f
oleksiychernenko286 <oleksiychernenko286@gmail.com>
2024-10-23 14:07:26 +0300

    add query params doc for /news and /categories

--- BE/src/categories/categories.controller.ts
+++ BE/src/categories/categories.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
-import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Category } from './category.entity';
 import { RequestService } from 'src/request/request.service';
 import { CacheService } from 'src/cache/cache.service';
@@ -17,6 +17,50 @@ export class CategoriesController {
     status: 200,
     description: 'Category',
     type: Category,
+  })
+  @ApiQuery({
+    name: 'includeNews',
+    required: false,
+    type: Boolean,
+    description:
+      'Include news in the response if set to true. If false or omitted, categories will not be included.',
+  })
+  @ApiQuery({
+    name: 'pagination[page]',
+    required: false,
+    type: Number,
+    description: 'Specify the page number to retrieve. Default is 1.',
+    example: 1,
+  })
+  @ApiQuery({
+    name: 'pagination[pageSize]',
+    required: false,
+    type: Number,
+    description: 'Specify the number of items per page. Default is 25.',
+    example: 25,
+  })
+  @ApiQuery({
+    name: 'pagination',
+    required: false,
+    type: String,
+    description:
+      'If set to "max", returns all the data without pagination. Overrides page and pageSize parameters.',
+    example: 'max',
+  })
+  @ApiQuery({
+    name: 'filters[field]',
+    required: false,
+    type: String,
+    description: 'The name of the field to filter data by (e.g., "name").',
+    example: 'name',
+  })
+  @ApiQuery({
+    name: 'filters[value]',
+    required: false,
+    type: String,
+    description:
+      'The value the specified filter field must match (e.g., "JS", "Sport", "Politics", or a numerical value).',
+    example: 'JS',
   })
   async getAll(
     @Query()
--- BE/src/news/news.enetity.ts
+++ BE/src/news/news.enetity.ts
@@ -1,4 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Category } from 'src/categories/category.entity';
+import { CreateCategoryDto } from 'src/categories/dto/create-category.dto';
 
 export class News {
   @ApiProperty({ example: 'Some Title', description: 'Title of new' })
@@ -8,11 +10,19 @@ export class News {
     example: 'Some text',
     description: 'Content can be string or markdown',
   })
-  content: {};
+  content: string;
 
   @ApiProperty({
-    example: '[1, 3, 5, 6]',
     description: 'Categories of new (array of categories id)',
+    example: {
+      id: 3,
+      documentId: 'o6x0w7j2hh81slkks66p6gus',
+      title: '1',
+      content: '1',
+      createdAt: '2024-10-22T13:17:13.637Z',
+      updatedAt: '2024-10-22T13:19:21.163Z',
+      publishedAt: '2024-10-22T13:19:21.178Z',
+    },
   })
-  categories: number[];
+  categories: {};
 }
