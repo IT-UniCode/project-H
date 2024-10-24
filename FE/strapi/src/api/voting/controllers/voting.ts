ca400bc509d63cd3d182a091e9872b961a7ae5a5
oleksiychernenko286 <oleksiychernenko286@gmail.com>
2024-10-24 17:58:49 +0300

    add /votings endpoint

---
+++ FE/strapi/src/api/voting/controllers/voting.ts
@@ -0,0 +1,7 @@
+/**
+ * voting controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::voting.voting');
---
+++ FE/strapi/src/api/voting/services/voting.ts
@@ -0,0 +1,7 @@
+/**
+ * voting service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::voting.voting');
