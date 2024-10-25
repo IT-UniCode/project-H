2357e83ebb395af6cdd444422fa09c12df310319
MasterOutLite <schurik458@gmail.com>
2024-10-25 10:52:40 +0300

    comments component, component for show comment list, comment service

--- FE/ui/src/components/Comments/Comments.tsx
+++ FE/ui/src/components/Comments/Comments.tsx
@@ -1,9 +1,15 @@
-function Comments() {
+import type { IComments } from "src/interfaces";
+
+export interface CommentsProps {
+  comment: IComments;
+}
+
+function Comments({ comment }: CommentsProps) {
   return (
     <section>
       <p>Name</p>
-      <p>Date created: </p>
-      <p>Content</p>
+      <p>Date created: {comment.createdAt} </p>
+      <p>{comment.content}</p>
     </section>
   );
 }
--- FE/ui/src/components/Comments/CommentsList.tsx
+++ FE/ui/src/components/Comments/CommentsList.tsx
@@ -1,10 +1,46 @@
-function CommentsList() {
-  const comments = ["sd"];
+import { useEffect, useState } from "preact/hooks";
+import Comments from "./Comments";
+import { TextField } from "@components/TextFields";
+import commentService from "@service/comment.service";
+import type { IComments } from "@interfaces/comments";
+import Button from "@components/Button";
+import type { FormEvent } from "preact/compat";
+
+export interface CommentsListProps {
+  documentType: string;
+  documentId: string;
+}
+
+function CommentsList({ documentId, documentType }: CommentsListProps) {
+  const [comments, setComments] = useState<IComments[]>([]);
+
+  async function getComments() {
+    const res = await commentService.getAll({ documentId, documentType });
+    setComments(res);
+  }
+
+  async function submitCreateComment(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const form = new FormData(event.currentTarget);
+    console.log("Form event", Array.from(form.entries()));
+  }
+
+  useEffect(() => {
+    getComments();
+  }, []);
 
   return (
-    <section>
-      // Textfields
-      {comments.map((v) => v)}
+    <section aria-label="comment block">
+      <form class="flex flex-col gap-y-4" onSubmit={submitCreateComment}>
+        <TextField type="text" placeholder="Comments" name="content" />
+        <Button class=" justify-self-end ml-auto">Submit</Button>
+      </form>
+
+      <section aria-label="comment list" class="flex flex-col gap-y-3 px-4">
+        {comments.map((v) => (
+          <Comments comment={v} />
+        ))}
+      </section>
     </section>
   );
 }
