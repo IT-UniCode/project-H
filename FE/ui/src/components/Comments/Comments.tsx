57267a3cb94df84afd531d22b5ff5089240bbdf4
MasterOutLite <schurik458@gmail.com>
2024-10-28 15:31:33 +0200

    updated comments

--- FE/ui/src/components/Comments/Comments.tsx
+++ FE/ui/src/components/Comments/Comments.tsx
@@ -5,11 +5,16 @@ export interface CommentsProps {
 }
 
 function Comments({ comment }: CommentsProps) {
+  const date = new Date(comment.createdAt);
+
   return (
-    <section>
-      <p>Name</p>
-      <p>Date created: {comment.createdAt} </p>
-      <p>{comment.content}</p>
+    <section class="border px-2 py-1 rounded-md">
+      <p class="text-xl">User: {comment.user.name}</p>
+      <p class="text-xs">
+        Date created: {date.toLocaleDateString()} {date.toLocaleTimeString()}
+      </p>
+      <div class="bg-black w-full h-[1px] my-1"></div>
+      <p class="text-xl">{comment.content}</p>
     </section>
   );
 }
--- FE/ui/src/components/Comments/CommentsList.tsx
+++ FE/ui/src/components/Comments/CommentsList.tsx
@@ -4,7 +4,8 @@ import { TextField } from "@components/TextFields";
 import commentService from "@service/comment.service";
 import type { IComments } from "@interfaces/comments";
 import Button from "@components/Button";
-import type { FormEvent } from "preact/compat";
+import { useAuth } from "@hooks/useAuth";
+import { useForm } from "@felte/preact";
 
 export interface CommentsListProps {
   documentType: string;
@@ -13,16 +14,25 @@ export interface CommentsListProps {
 
 function CommentsList({ documentId, documentType }: CommentsListProps) {
   const [comments, setComments] = useState<IComments[]>([]);
+  const { isAuth } = useAuth();
+  const { form } = useForm({
+    async onSubmit(values, context) {
+      if (isAuth && values.content) {
+        const res = await commentService.create({
+          content: values.content,
+          documentType,
+          documentId,
+        });
+        context.reset();
+        await getComments();
+      }
+    },
+  });
 
   async function getComments() {
     const res = await commentService.getAll({ documentId, documentType });
-    setComments(res);
-  }
 
-  async function submitCreateComment(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    const form = new FormData(event.currentTarget);
-    console.log("Form event", Array.from(form.entries()));
+    setComments(res.data);
   }
 
   useEffect(() => {
@@ -31,15 +41,16 @@ function CommentsList({ documentId, documentType }: CommentsListProps) {
 
   return (
     <section aria-label="comment block">
-      <form class="flex flex-col gap-y-4" onSubmit={submitCreateComment}>
-        <TextField type="text" placeholder="Comments" name="content" />
-        <Button class=" justify-self-end ml-auto">Submit</Button>
-      </form>
+      <p class="text-2xl mb-2">Comments</p>
+      {isAuth && (
+        <form ref={form} class="flex flex-col gap-y-4 mb-8">
+          <TextField type="text" placeholder="Comments" name="content" />
+          <Button class=" justify-self-end ml-auto">Submit</Button>
+        </form>
+      )}
 
       <section aria-label="comment list" class="flex flex-col gap-y-3 px-4">
-        {comments.map((v) => (
-          <Comments comment={v} />
-        ))}
+        {comments?.map((v) => <Comments comment={v} />)}
       </section>
     </section>
   );
