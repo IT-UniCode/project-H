3a6ff2e099320377ae0ae8339e510b3e2b88aea5
oleksiychernenko286 <oleksiychernenko286@gmail.com>
2024-10-30 17:16:24 +0200

    add global strapiImage interface

---
+++ BE/src/types/types/previewImage.ts
@@ -0,0 +1,40 @@
+export interface StrapiImage {
+  id: number;
+  documentId: string;
+  name: string;
+  alternativeText: any;
+  caption: any;
+  width: number;
+  height: number;
+  formats: ImageFormats;
+  hash: string;
+  ext: string;
+  mime: string;
+  size: number;
+  url: string;
+  previewUrl: any;
+  provider: string;
+  provider_metadata: any;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+}
+
+export interface ImageFormats {
+  small: ImageSize;
+  medium: ImageSize;
+  thumbnail: ImageSize;
+}
+
+export interface ImageSize {
+  ext: string;
+  url: string;
+  hash: string;
+  mime: string;
+  name: string;
+  path: any;
+  size: number;
+  width: number;
+  height: number;
+  sizeInBytes: number;
+}
