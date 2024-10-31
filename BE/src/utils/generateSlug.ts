5844c062c5bda656264b5a055130d7f8025e2bef
oleksiychernenko286 <oleksiychernenko286@gmail.com>
2024-10-31 11:52:51 +0200

    fix slug generation for forums

--- BE/src/utils/generateSlug.ts
+++ BE/src/utils/generateSlug.ts
@@ -1,5 +1,4 @@
 import { v4 } from 'uuid';
-
 export function generateSlug(title: string) {
-  return title.toLowerCase().split(' ').join('-') + '-' + v4();
+  return title.toLowerCase().split(' ').join('-') + '-' + v4().slice(0, 12);
 }
