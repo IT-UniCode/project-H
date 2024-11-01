829a5f2ab6405a6355bd2373f2555f269816a060
MasterOutLite <schurik458@gmail.com>
2024-11-01 10:44:44 +0200

    forum editor, style forum

--- FE/ui/src/components/Forum/ForumEditor.tsx
+++ FE/ui/src/components/Forum/ForumEditor.tsx
@@ -2,8 +2,9 @@ import { Button } from "@components/Button";
 import { TextField } from "@components/TextFields";
 import { useForm } from "@hooks/useForm";
 import forumService from "@service/forum.service";
-// import EasyMDE from "easymde";
+import { navigate } from "astro:transitions/client";
 import { useEffect, useRef } from "preact/compat";
+import { routes } from "src/routes";
 
 interface FormType {
   content: string;
@@ -15,23 +16,34 @@ function ForumEditor() {
   const { onSubmit, keys } = useForm<FormType>({
     values: { content: {}, title: {} },
     async onSubmit(values, context) {
-      console.log(values);
-      const res = await forumService.create(values);
-      console.log(res);
+      if (!values.content || !values.title) {
+        return;
+      }
+      try {
+        await forumService.create(values);
+        context.reset();
+        navigate(routes.Forum);
+      } catch (e) {}
     },
   });
 
   useEffect(() => {
-    if (editorRef.current) {
+    if (!editorRef.current) {
+      return;
+    }
+
+    if (!editorRef.current.style.display) {
       //@ts-ignore
-      new EasyMDE({ element: editorRef.current });
+      new EasyMDE({ element: editorRef.current, forceSync: true });
     }
   }, []);
 
   return (
     <form class="flex flex-col gap-y-3" onSubmit={onSubmit}>
-      <Button>Create</Button>
-      <TextField placeholder="Title" name={keys.title} />
+      <div class="flex gap-x-2">
+        <TextField placeholder="Title" name={keys.title} />
+        <Button>Create</Button>
+      </div>
       <textarea
         placeholder="Content"
         name={keys.content}
