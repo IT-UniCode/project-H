0886375b90e7ee23012c9102e5f27c31becd3ed8
MasterOutLite <schurik458@gmail.com>
2024-11-06 11:07:25 +0200

    drwer fixed animation and close

--- FE/ui/src/components/Drawer/Drawer.tsx
+++ FE/ui/src/components/Drawer/Drawer.tsx
@@ -1,49 +1,62 @@
 import clsx from "clsx";
-import { createPortal, useEffect, useRef, type ReactNode } from "preact/compat";
+import {
+  createPortal,
+  useEffect,
+  useRef,
+  useState,
+  type ReactNode,
+} from "preact/compat";
 
 export interface DrawerProps {
   open: boolean;
   children: ReactNode;
   class?: string;
+  close: () => void;
 }
 
-function Drawer({ open, children, class: className }: DrawerProps) {
-  const modalRef = useRef<HTMLDialogElement>(null);
+function Drawer({ open, children, class: className, close }: DrawerProps) {
   const container = document.getElementById("root");
+  const [isAnimating, setIsAnimating] = useState(false);
 
   useEffect(() => {
-    console.log(open);
-
+    let timer: NodeJS.Timeout;
     if (open) {
       const scrollbarWidth =
         window.innerWidth - document.documentElement.clientWidth;
-      //   modalRef.current?.showModal();
       document.body.classList.add("overflow-hidden");
       document.body.style.paddingRight = `${scrollbarWidth}px`;
+      setIsAnimating(true);
     } else {
-      //   modalRef.current?.close();
       document.body.classList.remove("overflow-hidden");
       document.body.style.paddingRight = "";
+      setTimeout(() => {
+        setIsAnimating(false);
+      }, 450);
     }
+
+    return () => clearTimeout(timer);
   }, [open]);
 
   if (!container) throw new Error("Not found container");
 
   return createPortal(
     <section
+      onClick={(event) => {
+        if (event.target === event.currentTarget) close();
+      }}
       class={clsx(
-        "fixed bg-gray-200 bg-opacity-30 w-full h-full rounded duration-500",
+        "fixed bg-gray-200 bg-opacity-30 w-full h-full rounded z-50",
         {
-          "translate-x-full": !open,
-          "translate-x-0": open,
+          "translate-x-full": !isAnimating,
+          "translate-x-0 ": isAnimating,
         },
         className,
       )}
     >
       <div
         class={clsx(
-          "absolute right-0 pointer-events-auto max-w-lg w-full  h-full bg-white shadow-xl transform transition-transform duration-500",
-          //   open ? "translate-x-0" : "translate-x-full", // animate slide-in
+          "absolute top-0 right-0 pointer-events-auto max-w-lg w-full h-full bg-white shadow-xl ",
+          "transform transition-transform duration-500",
           {
             "translate-x-full": !open,
           },
