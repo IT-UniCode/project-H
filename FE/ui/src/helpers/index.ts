0dc3f6e363a8b62e7923676bdd53c7e77db2a131
MasterOutLite <schurik458@gmail.com>
2024-11-06 16:49:51 +0200

    clamp

--- FE/ui/src/helpers/index.ts
+++ FE/ui/src/helpers/index.ts
@@ -3,6 +3,7 @@ export {
   confMarkdownForum,
   confMarkdownAsParagraph,
 } from "./configuration.markdown";
+export { clamp } from "./clamp";
 import { extractFirstTextAndImage } from "./extractors";
 import { useLocalStorage } from "./localStorageHelper";
 import { useSessionStorage } from "./sessionStorageHelper";
