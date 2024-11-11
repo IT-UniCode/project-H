644231f3bb5a68545a80fbc2fe3390efa8ab2499
MasterOutLite <schurik458@gmail.com>
2024-11-11 14:20:56 +0200

    chat added request to api

--- FE/ui/src/components/Chat/Chat.tsx
+++ FE/ui/src/components/Chat/Chat.tsx
@@ -1,48 +1,82 @@
+import { TextArea } from "@components/TextFields";
+import { useForm } from "@hooks/index";
+import type { ChatMessage } from "@interfaces/index";
+import chatService from "@service/chat.service";
 import clsx from "clsx";
-import { useState } from "preact/compat";
+import { useEffect, useState } from "preact/compat";
 
 export interface ChatProps {
-  chatId: string;
-  ownerChatId: string;
+  chatId: number;
+  userId: number;
   class?: string;
 }
 
-function Chat({ chatId, class: className, ownerChatId }: ChatProps) {
-  const [messages, setMessages] = useState<any[]>([
-    { message: "Hello, this is a chat!", ownerId: "df" },
-    { message: "This is a test message!", ownerId: "56" },
-    {
-      message:
-        "This is another test message! It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.",
-      ownerId: "df",
+interface Form {
+  message: string;
+}
+
+function Chat({ chatId, class: className, userId }: ChatProps) {
+  const [messages, setMessages] = useState<ChatMessage[]>([]);
+
+  const { keys, onSubmit } = useForm<Form>({
+    values: {
+      message: {},
     },
-  ]);
+    async onSubmit(values, context) {
+      if (!values.message) return;
+      const res = await chatService.sendMessage(chatId, values.message);
+      setMessages((prev) => [...prev, res]);
+      context.reset();
+    },
+  });
+
+  async function getMessages() {
+    const res = await chatService.getMesages(chatId);
+    setMessages(res);
+  }
+
+  useEffect(() => {
+    if (chatId <= 0) return;
+    getMessages();
+  }, [chatId]);
 
   return (
-    <section
-      class={clsx(
-        "flex-grow flex flex-col gap-y-2 px-1 py-1 overflow-y-auto",
-        className,
-      )}
-    >
-      {messages.map((v) => (
-        <article
-          class={clsx(
-            "flex flex-col bg-gray-200 w-fit py-1 px-2 rounded overflow-hidden max-w-[90%]",
-            { "ml-auto": v.ownerId === ownerChatId },
-          )}
-        >
-          <p>{v.message}</p>
-          <span
-            class={clsx("text-xs text-gray-500 select-none", {
-              "text-end": v.ownerId === ownerChatId,
-            })}
-          >
-            10:30
-          </span>
-        </article>
-      ))}
-    </section>
+    <>
+      <section class={clsx("flex-grow px-1 py-1 overflow-y-auto", className)}>
+        <div class="flex flex-col gap-y-2">
+          {messages.map((v) => (
+            <article
+              class={clsx("py-1 px-2 rounded overflow-hidden max-w-[90%]", {
+                "ml-auto bg-blue-200": v.userId === userId,
+                "bg-gray-200": v.userId !== userId,
+              })}
+            >
+              <p>{v.message}</p>
+              <span
+                class={clsx("text-xs text-gray-500 select-none", {
+                  "text-end": v.userId === userId,
+                })}
+              >
+                {new Date(v.createdAt).getHours().toString().padStart(2, "0")}:
+                {new Date(v.createdAt).getMinutes().toString().padStart(2, "0")}
+              </span>
+            </article>
+          ))}
+        </div>
+      </section>
+      <form
+        class="flex min-h-12 max-h-16 md:min-h-16 md:max-h-20 flex-grow bg-white rounded-t-md overflow-hidden"
+        onSubmit={onSubmit}
+      >
+        <TextArea
+          class={clsx("rounded-none h-full resize-none")}
+          name={keys.message}
+        />
+        <div class="bg-gray-300 flex items-center">
+          <button class="aspect-square h-10 text-xl">{">"}</button>
+        </div>
+      </form>
+    </>
   );
 }
 
--- FE/ui/src/components/Chat/ChatList.tsx
+++ FE/ui/src/components/Chat/ChatList.tsx
@@ -1,33 +1,123 @@
 import clsx from "clsx";
-import { useState } from "preact/hooks";
+import { useEffect, useMemo, useRef, useState } from "preact/hooks";
 import Chat from "./Chat";
-import { TextArea } from "@components/TextFields";
+import { TextArea, TextField } from "@components/TextFields";
+import userService from "@service/user.service";
+import type { IChat, User } from "@interfaces/index";
+import chatService from "@service/chat.service";
+import { useAuth } from "@hooks/useAuth";
+import { useForm } from "@hooks/useForm";
 
 export interface ChatListProps {
   class?: string;
 }
 
-function ChatList({ class: className }: ChatListProps) {
-  const [chat, setChat] = useState<any[]>([]);
+interface ChatState {
+  selected: number;
+  chats: IChat[];
+}
 
+function ChatList({ class: className }: ChatListProps) {
+  const [chat, setChat] = useState<ChatState>({ selected: -1, chats: [] });
+  const [search, setSearch] = useState("");
+  const { payload } = useAuth();
+
+  const [users, setUsers] = useState<{ search: User | null; open: boolean }>({
+    search: null,
+    open: false,
+  });
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  async function getChats() {
+    const res = await chatService.getChats();
+    setChat((prev) => ({ ...prev, chats: res }));
+  }
+
+  async function createChat() {
+    if (!users.search?.id) {
+      return;
+    }
+    const res = await chatService.createChat(users.search?.id);
+    setChat((prev) => ({ ...prev, chats: [...prev.chats, res] }));
+  }
+
+  useMemo(async () => {
+    if (!search) return;
+    const response = await userService.search(search);
+    setUsers((prev) => ({ ...prev, search: response }));
+  }, [search]);
+
+  useEffect(() => {
+    console.log(users);
+  }, [users]);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setUsers((prev) => ({ ...prev, open: false }));
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    getChats();
+
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
+  if (!payload) {
+    return <div>Loading...</div>;
+  }
   return (
-    <section class={clsx("flex max-w-5xl w-full mx-auto", className)}>
+    <section
+      class={clsx("flex max-w-5xl w-full mx-auto  max-h-[85vh]", className)}
+    >
       <section class="min-w-[100px] md:min-w-[250px] flex-[1_1_25%] bg-blue-200">
-        {chat.map((item) => (
-          <div key={item.id}>
-            <h3>{item.name}</h3>
-            <p>{item.message}</p>
-          </div>
-        ))}
+        <div class="relative" ref={dropdownRef}>
+          <TextField
+            class="bg-white rounded-none"
+            onChange={(e: any) => {
+              setSearch(e.target.value);
+            }}
+            onFocus={() => {
+              setUsers((prev) => ({ ...prev, open: true }));
+            }}
+          />
+          {/* {(users.search && users.open) + "d"} */}
+          {users.open && (
+            <div
+              class="absolute top-[100%] min-h-10 mt-2 left-0 right-0 bg-white shadow-md px-2 py-1"
+              onClick={createChat}
+            >
+              <h4 class="">Name: {users.search?.name}</h4>
+              <p class="text-xs text-gray-500">Email: {users.search?.email}</p>
+            </div>
+          )}
+        </div>
+
+        <section class="flex flex-col divide-y">
+          {chat.chats.map((item) => (
+            <div
+              key={item.id}
+              class="flex bg-white px-2 py-1 gap-x-2"
+              onClick={() => {
+                setChat((prev) => ({ ...prev, selected: item.id }));
+              }}
+            >
+              <div class="rounded-full bg-gray-400 flex-[1_1_20%]"></div>
+              <article class="flex-[2_1_80%]">
+                <h3>{item.firstUserId}</h3>
+                <p>{item.secondUserId}</p>
+              </article>
+            </div>
+          ))}
+        </section>
       </section>
       <section class="flex flex-col flex-[1_1_75%] bg-violet-300">
-        <Chat chatId="df" ownerChatId="56" />
-        <form class="flex min-h-12 max-h-16 md:min-h-16 md:max-h-20 flex-grow bg-white rounded-t-md overflow-hidden">
-          <TextArea class={clsx("rounded-none h-full resize-none")} />
-          <div class="bg-gray-300 flex items-center">
-            <button class="aspect-square h-10 text-xl">{">"}</button>
-          </div>
-        </form>
+        <Chat chatId={chat.selected} userId={payload?.sub} />
       </section>
     </section>
   );
